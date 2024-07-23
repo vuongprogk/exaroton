fa4f9e6014f57cf7427f0f133828f6cbf45ffecc
ace <vuong753333@gmail.com>
2024-07-23 23:04:39 +0700

    Update status

--- app.js
+++ app.js
@@ -3,47 +3,48 @@ import { Client, GatewayIntentBits } from "discord.js";
 import * as discord from "./utils.js";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-let status = await discord.getStatus();
 let address = await discord.getAddress();
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 // Custom command
 async function start(interaction) {
-  await discord.startServer();
-  status = 1;
+  await discord.startServer;
   await interaction.reply(`Server is starting at ${address}`);
 }
 async function stop(interaction) {
   await discord.stopServer();
-  status = 0;
   await interaction.reply("Server is stopping");
 }
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  status = await discord.getStatus();
   if (interaction.commandName === "server") {
+    let status = await discord.getStatus();
     if (status === 0) {
       await start(interaction);
     } else if (status === 1) {
       await stop(interaction);
     }
   } else if (interaction.commandName === "startserver") {
+    let status = await discord.getStatus();
     if (status === 0) {
       await start(interaction);
     } else await interaction.reply(`Server is running at ${address}`);
   } else if (interaction.commandName === "stopserver") {
+    let status = await discord.getStatus();
     if (status === 1) {
       await stop(interaction);
     } else await interaction.reply("Server is not running");
   } else if (interaction.commandName === "executecmd") {
+    let status = await discord.getStatus();
     if (status === 1) {
       let cmd = interaction.options.getString("command");
       await discord.execCommand(cmd);
     } else await interaction.reply("Server is not running");
   } else if (interaction.commandName === "serverstatus") {
+    let status = await discord.getStatus();
     await interaction.reply(discord.handleStatus(status));
   } else if (interaction.commandName === "getserveraddr") {
     await interaction.reply(address);
